065ece7964ab77b604bfa7b33c713f367d0886d4
Ezgiackgz <75390977+Ezgiackgz@users.noreply.github.com>
2022-04-23 01:16:29 +0300

    first commit

---
+++ css/app.js
@@ -0,0 +1,25 @@
+new Vue({
+  el: "#app",
+  data: {
+    player: 100,
+    monster: 100,
+    game : false
+  },
+  methods: {
+  startGame: function () {
+    this.game = true;
+    },
+    attect: function () {
+
+    },
+    privateAttect: function () {
+  
+    },
+    firstAid: function () {}
+    ,
+    giveUp: function () {
+  
+    },
+  }
+
+});
