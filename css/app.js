31497f90f00d0099403c76ec92f5dc2b79ac543a
Ezgiackgz <75390977+Ezgiackgz@users.noreply.github.com>
2022-04-23 22:22:33 +0300

    button fonksiyonları eklendi

--- css/app.js
+++ css/app.js
@@ -10,16 +10,48 @@ new Vue({
     this.game = true;
     },
     attect: function () {
-
+        /* Saldırı zamanında can azaltmak için random sayı oluştur*/
+        var point = Math.ceil(Math.random() * 10);
+        /* oyuncunun canından eksilt*/
+        this.monster-=point;
+        /*Canavarın Canını azaltma konksiyonunu çalıştır */
+        this.monsterAttect();
+        console.log(this.monster);
+        console.log(this.player);
     },
     privateAttect: function () {
-  
+            /* Saldırı zamanında can azaltmak için random sayı oluştur*/
+            var point = Math.ceil(Math.random() * 50);
+            /* oyuncunun canından eksilt*/
+            this.monster-=point;
+            /*Canavarın Canını azaltma konksiyonunu çalıştır */
+            this.monsterAttect();
+            console.log(this.monster);
+            console.log(this.player);
     },
-    firstAid: function () {}
+    firstAid: function () {
+         /* Saldırı zamanında can azaltmak için random sayı oluştur*/
+         var point = Math.ceil(Math.random() * 50);
+         /* oyuncunun canından eksilt*/
+         this.monster+=point;
+         /*Canavarın Canını azaltma konksiyonunu çalıştır */
+         this.monsterAttect();
+         console.log(this.monster);
+         console.log(this.player);
+    }
+    
     ,
     giveUp: function () {
-  
+        this.player = 0;
+        console.log(this.monster);
+        console.log(this.player);
     },
+    /*Canavar İçin saldırı fonksiyonu */
+    monsterAttect:function(){
+        var point = Math.ceil(Math.random()*15);
+        this.player-=point;
+        
+    }
   }
 
 });
